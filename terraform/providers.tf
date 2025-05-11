2a4ac571a770fc589778c7ec04ed5e2a3645658b
Dave Kadziola <194847688+DaveKadziola@users.noreply.github.com>
2025-05-11 17:54:10 +0200

    fixed api token

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,5 @@
 provider "proxmox" {
   endpoint = var.proxmox_api_url
   insecure = true
-  api_token = "${var.proxmox_api_user}@pve!${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
+  api_token = "${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
 }
