0a93815f96b6987f965835bd018d6fc8fe0bf700
Dave Kadziola <194847688+DaveKadziola@users.noreply.github.com>
2025-06-28 16:45:07 +0200

    reverted unsecured conn

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,6 @@
 provider "proxmox" {
   endpoint  = var.proxmox_api_url
-  insecure  = false
+  insecure  = true
   api_token = "${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
   ssh {
     agent    = true
